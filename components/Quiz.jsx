b38543f23bab86a1806e4bbf1b9aaf2d8dab535e
BithunT-Nair <react666cr@gmail.com>
2025-03-29 13:26:40 +0530

    improvements

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -6,37 +6,42 @@ const Quiz = () => {
   const [index, setIndex] = useState(0);
   const [color, setColor] = useState('default');
   const [isClicked, setIsClicked] = useState(false);
-  const [disabled, setDisabled] = useState(false)
+  const [disabled, setDisabled] = useState(false);
+  // const [prevDisabled, setPrevDisabled] = useState(false)
   const [score, setScore] = useState(0);
+  const [correctAnswer, setCorrectAnswer] = useState('')
 
   const handleNext = () => {
-    if (index == 8) {
+    if (index === 8) {
       setDisabled(true)
     }
     setColor('default');
-    setIsClicked(false)
+    setIsClicked(false);
     setIndex(index + 1);
   }
 
   const handlePrev = () => {
-    if (index == 0) {
-      setDisabled(true)
+    if (index > 0) {
+      setIndex(index - 1);
+      console.log(index);
+      setDisabled(false)
+      setColor('default');
+      setIsClicked(false)
     }
-    setIndex(index - 1);
-    setColor('default')
+    // if (index - 1 === 0) {
+    //   setPrevDisabled(true)
+    // }
   };
   const aIsCorrect = (e) => {
-
-
     console.log(e.target.value);
     const answer = e.target.value;
     const id = e.target.id
-
     if (answer === data[index].correctAnswer && id == 1) {
       setColor('green_a');
       setScore(score + 1);
     } else {
-      setColor('red_a')
+      setColor('red_a');
+      checkAnswer(data[index].options)
     };
     setIsClicked(true)
   };
@@ -52,7 +57,8 @@ const Quiz = () => {
       setColor('green_b');
       setScore(score + 1);
     } else {
-      setColor('red_b')
+      setColor('red_b');
+      checkAnswer(data[index].options)
     };
     setIsClicked(true)
   };
@@ -63,7 +69,8 @@ const Quiz = () => {
       setColor('green_c');
       setScore(score + 1);
     } else {
-      setColor('red_c')
+      setColor('red_c');
+      checkAnswer(data[index].options)
     };
     setIsClicked(true)
   };
@@ -74,15 +81,33 @@ const Quiz = () => {
       setColor('green_d');
       setScore(score + 1);
     } else {
-      setColor('red_d')
+      setColor('red_d');
+      checkAnswer(data[index].options);
     };
     setIsClicked(true)
   };
+  const checkAnswer = (options) => {
+    console.log(options);
+    console.log(data);
 
+    let crctAnswer = ''
+    for (let i = 0; i < options.length; i++) {
+      if (data[index].correctAnswer === options[i]) {
+        crctAnswer = i;
+        // setCorrectAnswer(crctAnswer)
+      }
+    }
+    console.log(crctAnswer);
+    displayAnswer(crctAnswer)
 
-
-
-
+  };
+  const displayAnswer=(answer)=>{
+    if(answer==0){
+      setColor('green_a')
+    }else if(answer==1){
+      setColor('green_b')
+    }
+  }
   return (
     <>
       <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-black via-yellow-300 to-red-400 p-4">
@@ -95,25 +120,25 @@ const Quiz = () => {
           </h2>
           {/* Options */}
           <div className="mt-6 space-y-3">
-            <button disabled={isClicked} className={`w-full px-4 py-3 border rounded-md text-left ${color === 'green_a' ? "bg-green-400" : color === "red_a" ? "bg-red-400" : "bg-gray-50"} hover:cursor-pointer`}
+            <button disabled={isClicked} className={`w-full px-4 py-3 border rounded-md text-left ${color === 'green_a' ? "bg-green-400  animate-pulse border-none" : color === "red_a" ? "bg-red-400  animate-pulse border-none" : "bg-gray-50"} hover:cursor-pointer`}
               id={1}
               value={data[index].options[0]}
               onClick={aIsCorrect} >
               A. {data[index].options[0]}
             </button>
-            <button disabled={isClicked} className={`w-full px-4 py-3 border rounded-md text-left ${color === 'green_b' ? "bg-green-400" : color === "red_b" ? "bg-red-400" : "bg-gray-50"} hover:cursor-pointer`}
+            <button disabled={isClicked} className={`w-full px-4 py-3 border rounded-md text-left ${color === 'green_b' ? "bg-green-400  animate-pulse border-none" : color === "red_b" ? "bg-red-400  animate-pulse border-none" : "bg-gray-50"} hover:cursor-pointer`}
               id={2}
               onClick={bIsCorrect}
               value={data[index].options[1]}>
               B. {data[index].options[1]}
             </button>
-            <button disabled={isClicked} className={`w-full px-4 py-3 border rounded-md text-left ${color === 'green_c' ? "bg-green-400" : color === "red_c" ? "bg-red-400" : "bg-gray-50"} hover:cursor-pointer`}
+            <button disabled={isClicked} className={`w-full px-4 py-3 border rounded-md text-left ${color === 'green_c' ? "bg-green-400  animate-pulse border-none" : color === "red_c" ? "bg-red-400  animate-pulse border-none" : "bg-gray-50"} hover:cursor-pointer`}
               id={3}
               onClick={cIsCorrect}
               value={data[index].options[2]} >
               C. {data[index].options[2]}
             </button>
-            <button disabled={isClicked} className={`w-full px-4 py-3 border rounded-md text-left ${color === 'green_d' ? "bg-green-400" : color === "red_d" ? "bg-red-400" : "bg-gray-50"} hover:cursor-pointer`}
+            <button disabled={isClicked} className={`w-full px-4 py-3 border rounded-md text-left ${color === 'green_d' ? "bg-green-400  animate-pulse border-none" : color === "red_d" ? "bg-red-400  animate-pulse border-none" : "bg-gray-50"} hover:cursor-pointer`}
               id={4}
               onClick={dIsCorrect}
               value={data[index].options[3]}>
@@ -124,14 +149,14 @@ const Quiz = () => {
           <div className="mt-6 flex justify-between items-center">
             <button className="px-6 py-3 text-white font-semibold bg-blue-600 hover:bg-blue-700 rounded-md transition hover:cursor-pointer"
               onClick={handlePrev}
-              disabled={disabled}>
+              disabled={''}>
               Previous
             </button>
 
             {/* Score Section Centered */}
             <div className="flex-grow flex items-center justify-center">
               <div className="text-black font-semibold text-lg bg-green-300 px-4 py-2 rounded-md shadow-md">
-                Score: {score}
+                Score: {score}{'/10'}
               </div>
             </div>
 
